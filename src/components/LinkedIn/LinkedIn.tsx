ae21389dc007291ef68dde213d85adec9da8f995
Luke Runyon <runyonluke@gmail.com>
2023-11-25 08:41:15 -0600

    V 0.3.1 Beginnings of LinkedIn component

---
+++ src/components/LinkedIn/LinkedIn.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./_linkedIn.module.scss";
+
+export default function LinkedIn(): React.ReactElement {
+  return <div className={styles.container}></div>;
+}
---
+++ src/components/LinkedIn/_linkedIn.module.scss.d.ts
@@ -0,0 +1,9 @@
+export declare const app: string;
+export declare const container: string;
+export declare const fontLarge: string;
+export declare const fontMedium: string;
+export declare const fontSmall: string;
+export declare const fontXlarge: string;
+export declare const fontXsmall: string;
+export declare const fontXxlarge: string;
+export declare const landing: string;
